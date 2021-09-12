3e55552786af104cee1fe3fa15d8815256cc2ad1
Lukas Kurz <vb.net@kurzweb.de>
2021-09-12 17:08:49 +0200

    Added Gain Node

---
+++ VBAudioRooter/Controls/GainNodeControl.xaml.vb
@@ -0,0 +1,47 @@
+﻿
+Imports VBAudioRooter.AudioGraphControl
+Imports VBAudioRooter.Utils
+Imports Windows.Media.Audio
+
+Namespace Controls
+
+    Public NotInheritable Class GainNodeControl
+        Inherits UserControl
+        Implements IAudioNodeControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+#Region "Identity"
+        Public ReadOnly Property ID As Guid = Guid.NewGuid() Implements IAudioNodeControl.ID
+        Public ReadOnly Property NodeType As NodeTypeEnum Implements IAudioNodeControl.NodeType
+            Get
+                Return NodeTypeEnum.Effect
+            End Get
+        End Property
+        Public Property Canvas As Canvas Implements IAudioNodeControl.Canvas
+        Public ReadOnly Property BaseAudioNode As IAudioNode Implements IAudioNodeControl.BaseAudioNode
+
+        Public ReadOnly Property OutgoingConnector As ConnectorControl Implements IAudioNodeControl.OutgoingConnector
+            Get
+                Return OutgoingConnectorControl
+            End Get
+        End Property
+#End Region
+
+        Public Async Function Initialize(graph As AudioGraph) As Task Implements IAudioNodeControl.Initialize
+            _BaseAudioNode = graph.CreateSubmixNode()
+
+            RadialGauge.Value = BaseAudioNode.OutgoingGain
+        End Function
+
+        Public Sub OnStateChanged(state As GraphState) Implements IAudioNodeControl.OnStateChanged : End Sub
+
+        Private Sub RadialGauge_ValueChanged(sender As Object, e As RangeBaseValueChangedEventArgs)
+            BaseAudioNode.OutgoingGain = RadialGauge.Value
+        End Sub
+
+    End Class
+
+End Namespace
