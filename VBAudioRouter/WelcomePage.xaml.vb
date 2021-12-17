5e828fb59dcd1b53ad5010ab78e6ee088b42dbd6
Lukas Kurz <vb.net@kurzweb.de>
2021-12-17 22:06:05 +0100

    Use recommended transition

--- VBAudioRouter/WelcomePage.xaml.vb
+++ VBAudioRouter/WelcomePage.xaml.vb
@@ -4,17 +4,20 @@ Public NotInheritable Class WelcomePage
     Inherits Page
 
     Private Sub NavigationView_SelectionChanged(sender As Microsoft.UI.Xaml.Controls.NavigationView, args As Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs)
+        Dim navOptions As New FrameNavigationOptions()
+        navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo
+
         Select Case DirectCast(DirectCast(args.SelectedItem, Microsoft.UI.Xaml.Controls.NavigationViewItem).Tag, String)
             Case "Home"
                 Throw New NotImplementedException()
             Case "Edit"
-                ContentFrame.Navigate(GetType(GraphViewPage))
+                ContentFrame.NavigateToType(GetType(GraphViewPage), Nothing, navOptions)
             Case "AudioControl"
-                ContentFrame.Navigate(GetType(AudioControlPage))
+                ContentFrame.NavigateToType(GetType(AudioControlPage), Nothing, navOptions)
             Case "VirtualDevices"
                 Throw New NotImplementedException()
             Case "Help"
-                ContentFrame.Navigate(GetType(HelpPage))
+                ContentFrame.NavigateToType(GetType(HelpPage), Nothing, navOptions)
         End Select
     End Sub
 
