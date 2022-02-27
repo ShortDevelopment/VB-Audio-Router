fe5f3dbcdd7d02eb953ab7b0f800968eb955638b
Lukas Kurz <lukas@shortdev.de>
2022-02-27 19:00:57 +0100

    UnloadLib (Not working yet)

--- UncloakHelper/dllmain.cpp
+++ UncloakHelper/dllmain.cpp
@@ -14,8 +14,11 @@ public:
 
 };
 
+HMODULE currentModule;
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
+	currentModule = hModule;
 	CoInitialize(NULL);
 	return TRUE;
 }
@@ -46,23 +49,22 @@ PINVOKE void UnCloakWindowShell(HWND hWnd) {
 	HRESULT hres = 0;
 	IServiceProvider* serviceProvider;
 	CLSID CLSID_ImmersiveShell = { 0xc2f03a33,0x21f5,0x47fa,{0xb4,0xbb,0x15,0x63,0x62,0xa2,0xf2,0x39} };
-	MessageBoxW(nullptr, L"CoCreateInstance", L"Information", MB_OK);
 	hres = CoCreateInstance(CLSID_ImmersiveShell, nullptr, CLSCTX_ALL, __uuidof(IServiceProvider), (void**)&serviceProvider);
 	if (hres == 0) {
-		MessageBoxW(nullptr, L"QueryService", L"Information", MB_OK);
 		IUnknown* immersiveApplication;
 		IID IID_IImmersiveApplicationManager = { 0xbf63999f,0x7411,0x40da,{0x86,0x1c,0xdf,0x72,0xc0,0xff,0xee,0x84} };
 		hres = serviceProvider->QueryService(IID_IImmersiveApplicationManager, IID_IImmersiveApplicationManager, (void**)&immersiveApplication);
 		if (hres == 0) {
 			IUncloakWindowService* uncloakService;
-			MessageBoxW(nullptr, L"QueryInterface", L"Information", MB_OK);
 			hres = immersiveApplication->QueryInterface(&uncloakService);
 			if (hres == 0) {
-				MessageBoxW(nullptr, L"UncloakWindow", L"Information", MB_OK);
 				hres = uncloakService->UncloakWindow(hWnd);
 			}
 		}
 	}
-	DisplayNumber(hres);
 	ExitThread(hres);
+}
+
+PINVOKE void UnloadLib() {
+	FreeLibrary(currentModule);
 }
