a069aabe0bbb73a6cd9572e8117f77ee447d27d5
Lukas Kurz <lukas@shortdev.de>
2022-04-08 15:16:13 +0200

    Fix for x86

--- XamlIslands/FullTrustUWP/Core/Activation/CoreWindowActivator.cs
+++ XamlIslands/FullTrustUWP/Core/Activation/CoreWindowActivator.cs
@@ -7,7 +7,7 @@ namespace FullTrustUWP.Core.Activation
 {
     public static class CoreWindowActivator
     {
-        public enum WindowType : long
+        public enum WindowType : int
         {
             IMMERSIVE_BODY = 0,
             IMMERSIVE_DOCK,
@@ -27,14 +27,15 @@ namespace FullTrustUWP.Core.Activation
             uint width,
             uint height,
             uint dwAttributes,
-            IntPtr hOwnerWindow,
-            Guid riid,
+            ref IntPtr hOwnerWindow,
+            ref Guid riid,
             out ICoreWindowInterop windowRef
         );
 
         public static CoreWindow CreateCoreWindow(WindowType windowType, string windowTitle, IntPtr hOwnerWindow, int x = 0, int y = 0, uint width = 10, uint height = 10, uint dwAttributes = 0)
         {
-            Marshal.ThrowExceptionForHR(PrivateCreateCoreWindow(windowType, windowTitle, x, y, width, height, dwAttributes, hOwnerWindow, typeof(ICoreWindowInterop).GUID, out ICoreWindowInterop windowRef));
+            Guid iid = typeof(ICoreWindowInterop).GUID;
+            Marshal.ThrowExceptionForHR(PrivateCreateCoreWindow(windowType, windowTitle, x, y, width, height, dwAttributes, ref hOwnerWindow, ref iid, out ICoreWindowInterop windowRef));
             return (windowRef as object as CoreWindow)!;
         }
     }
