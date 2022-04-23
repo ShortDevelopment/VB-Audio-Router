83ebd194cde1145cb1a3fc3826f919d39cfff7c3
Lukas Kurz <lukas@shortdev.de>
2022-04-23 17:05:09 +0200

    Split repos

---
+++ VBAudioRouter.Host/Program.cs
@@ -0,0 +1,14 @@
+﻿using FullTrustUWP.Core.Xaml;
+using System;
+
+namespace VBAudioRouter.Host
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            XamlApplicationWrapper.Run<App, WelcomePage>();
+        }
+    }
+}
